30cf4e36a831b64c3ceb2e2be136210d6d27d165
repenaattori <reimarii@oamk.fi>
2025-12-23 17:21:56 +0200

    init

---
+++ code.js
@@ -0,0 +1,59 @@
+let key = '';
+let qnum = 1;
+let index = 0;
+
+document.addEventListener('keyup', updateKey);
+let wordDiv = document.getElementById('word');
+let qDiv = document.querySelector('h2');
+
+function updateKey(e){
+    let letters = document.querySelectorAll('.letter');
+    letters[index].firstChild.textContent = e.key.toUpperCase();
+
+    let matches = 0;
+    for (let i = 0; i < letters.length; i++) {
+        if(questions[qnum].a[i].toUpperCase() == letters[i].textContent.toUpperCase()){
+            matches++;
+        }
+    }
+
+    
+    if(matches==questions[qnum].a.length){
+        letters.forEach(l => l.classList.add('correct'))
+    }
+
+}
+
+setWord();
+
+function setWord(){
+    qDiv.textContent = questions[qnum].q;
+    for (let i = 0; i < questions[qnum].a.length; i++) {
+        let div = document.createElement('div');
+        div.classList.add('letter');
+        div.addEventListener('click',()=> letterClick(i));
+        let span = document.createElement('span');
+        div.appendChild(span);
+        wordDiv.appendChild(div);        
+    }
+}
+
+
+function letterClick(ind){
+    let letters = document.querySelectorAll('.letter');
+    for (let i = 0; i < letters.length; i++) {
+        if(ind==i){
+            letters[i].classList.add('selected');
+        }   
+        else{
+            letters[i].classList.remove('selected')
+        }
+    }
+
+    index = ind;
+}
+
+
+function addCorrectLetter(){
+
+}
---
+++ questions.js
@@ -0,0 +1,4 @@
+let questions = [
+    {q:'Muistetaankohan meitä vielä, kun ? leijaillaan tiellä', a: 'tomuna', letter: 'O'},
+    {q: 'Parjunkissan kirkas väri kotona?', a:'oranssi', letter: 'R'}
+]
