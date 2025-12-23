43f755dd429a5bb842f5b71df9035937ba9053cb
repenaattori <reimarii@oamk.fi>
2025-12-24 00:14:00 +0200

    uutta

--- questions.js
+++ questions.js
@@ -1,33 +1,18 @@
 let leinolle = [
     {q:'Muistetaankohan meitä vielä, kun ? leijaillaan tiellä', a: 'tomuna', letter: 'O'},
     {q: 'Meidän pajunkissan kirkas väri', a:'oranssi', letter: 'R'},
-    {
-        q: 'Aakkosia on yhteensä 29. Mikä kirjain oon aakkosten kirjain, jonka järjestysnumero on laskutoimituksen 51-22 tulos?', 
-        a:'Ö', 
-        letter: 'Ö'
-    },
-    {
-        q: 'Lue nollia!! Siitä paljastuu kaverusten etunimet. Kuka on se toinen?',
-        a: 'allu',
-        letter: 'A'
-    }
+    {q: '"Lue nollia". Siinäpä paljastui teidän kaverusten etunimet. Kuka on se toinen?',a: 'allu',letter: 'A'},
+    {q: 'Mikä my little pony äidillä oli pienenä?', a: 'rarity', letter:'ä'},
+    {q: 'Kuka on eksynyt New Yorkissa?', a: 'kevin', letter:'v'},
+    {q: `Rock 'n roll ? bring thine hard rock hallelujah?`, a: 'angels', letter:'A'}
 ]
 
 
 let maunolle = [
     {q: 'Skuutilla hyppy siten, että lauta pyörähtää kerran alla', a:'tailwhip', letter: 'T'},
     {q: 'Mikäs se hurjistuu, kun silmät päässä pullistuu?', a:'kampiakseli', letter: 'P'},
-    {q: 'Kukka keittiön pöydällä', a:'kampiakseli', letter: 'R'},
-    {
-        q: 'Aakkosia on yhteensä 29. Mikä kirjain oon aakkosten kirjain, jonka järjestysnumero on laskutoimituksen 51-22 tulos?', 
-        a:'Ö', 
-        letter: 'Ö'
-    }
+    {q: 'Kukka keittiön pöydällä', a:'ruusu', letter: 'R'},
+    {q: 'Aakkosia on yhteensä 29. Mikä kirjain oon aakkosten kirjain, jonka järjestysnumero on laskutoimituksen 51-22 tulos?', a:'Ö', letter: 'Ö'},
+    {q: 'Mikä on yhdeksäs kuukausi?', a:'syyskuu',letter:'Y'},
+    {q: 'Montako olkipukkia on joulukuusessa?', a: 'kolme', letter: 's'}
 ]
-
-
-
-
-let y = 'ortpröa'
-
-let x ='röypäavarots';
