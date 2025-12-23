cee866b40cf8535bc7559b395c7c1a78b54b2eaf
repenaattori <reimarii@oamk.fi>
2025-12-23 17:44:40 +0200

    input updates

--- code.js
+++ code.js
@@ -7,20 +7,20 @@ let wordDiv = document.getElementById('word');
 let qDiv = document.querySelector('h2');
 
 function updateKey(e){
-    let letters = document.querySelectorAll('.letter');
-    letters[index].firstChild.textContent = e.key.toUpperCase();
+    // let letters = document.querySelectorAll('.letter');
+    // letters[index].firstChild.textContent = e.key.toUpperCase();
 
-    let matches = 0;
-    for (let i = 0; i < letters.length; i++) {
-        if(questions[qnum].a[i].toUpperCase() == letters[i].textContent.toUpperCase()){
-            matches++;
-        }
-    }
+    // let matches = 0;
+    // for (let i = 0; i < letters.length; i++) {
+    //     if(questions[qnum].a[i].toUpperCase() == letters[i].textContent.toUpperCase()){
+    //         matches++;
+    //     }
+    // }
 
     
-    if(matches==questions[qnum].a.length){
-        letters.forEach(l => l.classList.add('correct'))
-    }
+    // if(matches==questions[qnum].a.length){
+    //     letters.forEach(l => l.classList.add('correct'))
+    // }
 
 }
 
@@ -29,18 +29,18 @@ setWord();
 function setWord(){
     qDiv.textContent = questions[qnum].q;
     for (let i = 0; i < questions[qnum].a.length; i++) {
-        let div = document.createElement('div');
-        div.classList.add('letter');
-        div.addEventListener('click',()=> letterClick(i, div));
-        let span = document.createElement('span');
-        div.appendChild(span);
-        wordDiv.appendChild(div);        
+        let input = document.createElement('input');
+        input.classList.add('letter');
+        input.addEventListener('click',()=> letterClick(i));
+        wordDiv.appendChild(input);        
+
+        //input.addEventListener('input', e => console.log(e));
+        input.setAttribute('maxlength', "1")
     }
 }
 
 
-function letterClick(ind, div){
-    div.focus();
+function letterClick(ind){
     let letters = document.querySelectorAll('.letter');
     for (let i = 0; i < letters.length; i++) {
         if(ind==i){
